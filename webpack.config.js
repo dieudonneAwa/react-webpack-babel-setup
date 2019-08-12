cd44754f503269d5d726d0ef50448f094e3da16a
awadieudone <awadieudone@gmail.com>
2019-08-12 11:13:54 +0100

    set up an empty react project
    Set up babel, prettier, webpack 4 and eslint using airbnb guide

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'index_module.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader'
+					}
+				]
+			},
+			{
+				test: /\.css$/,
+				use: [ 'style-loader', 'css-loader' ]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: './src/index.html',
+			filename: './index.html'
+		})
+	]
+};
